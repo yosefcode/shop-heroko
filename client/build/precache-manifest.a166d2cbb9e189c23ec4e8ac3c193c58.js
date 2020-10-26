d7335ab8fc86f4fed76be264bc1bd93dd9360a04
yosefcode <67313446+yosefcode@users.noreply.github.com>
2020-10-27 00:28:00 +0200

    client

---
+++ client/build/precache-manifest.a166d2cbb9e189c23ec4e8ac3c193c58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec1ebd69f68f04ecaebe0212817e43c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "130de16f00c98549e416",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "a26144d48b65c142bbf1",
+    "url": "/static/css/main.e9dcfa4e.chunk.css"
+  },
+  {
+    "revision": "130de16f00c98549e416",
+    "url": "/static/js/2.51c46265.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.51c46265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26144d48b65c142bbf1",
+    "url": "/static/js/main.649ffe49.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c7a5cce3a7b1d936b9383f57b4546da4",
+    "url": "/static/media/cart.c7a5cce3.jpg"
+  },
+  {
+    "revision": "6922e80f631123da3b564004d6a11847",
+    "url": "/static/media/store.6922e80f.png"
+  }
+]);
