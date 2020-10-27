fc2abf8eb9a72a24395e41a7b8a31e2acbdf563e
yosefcode <67313446+yosefcode@users.noreply.github.com>
2020-10-27 11:21:04 +0200

    proxy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client/build")));
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 7000 ;
 
 connectToDb().then(async () => {
   app.listen(port, () => {
