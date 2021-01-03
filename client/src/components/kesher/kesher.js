afd286648ef28a19fc61e7e347ae4c04c24a1f46
yosefcode <67313446+yosefcode@users.noreply.github.com>
2021-01-03 03:03:20 +0200

    03/01

--- client/src/components/kesher/kesher.js
+++ client/src/components/kesher/kesher.js
@@ -7,7 +7,7 @@ function Kesher() {
 
   const addProduct = () => {
     axios
-      .post(`${process.env.REACT_APP_URL}/send-mail/`, infoKesher)
+      .post(`${process.env.REACT_APP_URL}send-mail/`, infoKesher)
       .then((res) => console.log("res.data"));
 
     document.getElementById("name").value = "";
