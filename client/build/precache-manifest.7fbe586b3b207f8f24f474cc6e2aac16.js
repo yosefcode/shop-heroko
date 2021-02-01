dca301e95c2254f5eb51d978fb6c92a5cda579b6
yosefcode <67313446+yosefcode@users.noreply.github.com>
2021-02-01 21:02:59 +0200

    zz

---
+++ client/build/precache-manifest.7fbe586b3b207f8f24f474cc6e2aac16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93cb49b78449bf2e3c16c30018fea32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fc14047fc773c6f4909",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "cecdfe5e2b1bd580e47b",
+    "url": "/static/css/main.8c600118.chunk.css"
+  },
+  {
+    "revision": "0fc14047fc773c6f4909",
+    "url": "/static/js/2.2445b722.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.2445b722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cecdfe5e2b1bd580e47b",
+    "url": "/static/js/main.0c1182b2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a22ce11021582665b3969e3c385de6f7",
+    "url": "/static/media/WhatsApp.a22ce110.png"
+  },
+  {
+    "revision": "bc503c35702112f77cf786514ce23fed",
+    "url": "/static/media/call.bc503c35.svg"
+  },
+  {
+    "revision": "c7a5cce3a7b1d936b9383f57b4546da4",
+    "url": "/static/media/cart.c7a5cce3.jpg"
+  },
+  {
+    "revision": "640fc0ea173342af3929fbe2c559bb5f",
+    "url": "/static/media/mail.640fc0ea.png"
+  },
+  {
+    "revision": "6922e80f631123da3b564004d6a11847",
+    "url": "/static/media/store.6922e80f.png"
+  }
+]);
