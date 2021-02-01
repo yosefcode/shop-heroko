9f7d0075cf194826f79d5449750074daf38c41d6
yosefcode <67313446+yosefcode@users.noreply.github.com>
2021-02-01 02:47:48 +0200

    build

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.json());
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build"));
 });
 
 const PORT = process.env.PORT || 7000;
