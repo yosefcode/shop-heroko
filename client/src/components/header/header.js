7bb08f09494c6d11d016918adfd0d5af4677cf9c
yosefcode <67313446+yosefcode@users.noreply.github.com>
2023-06-06 22:46:35 +0300

    cng num

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -9,21 +9,36 @@ const Header = () => {
   return (
     <div className="header">
       <h3>רהיטי איכות</h3>
-      <h5>איזור התעשייה שער בנימין</h5>
+      <h5>רחוב המעפילים 5 עפולה</h5>
       <ion-icon name="logo-youtube"></ion-icon>
       <ion-icon src="/path/to/external/file.svg"></ion-icon>
       <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
-      <h5>058-411-2424</h5>
+      <h5>050-123-4567</h5>
 
-      <img className="what" src={what} alt="תמונה חסרה" onClick={ ()=>{
-    window.open ( "https://wa.me/972584112424/")
-}}/>
-      <img className="call" src={call} alt="תמונה חסרה" onClick={ ()=>{
-    window.open ( "tel:0584112424")
-}}/>
-      <img className="mail" src={mail} alt="תמונה חסרה" onClick={ ()=>{
-    window.open ( "mailto:yosef9987@gmail.com")
-}}/>
+      <img
+        className="what"
+        src={what}
+        alt="תמונה חסרה"
+        onClick={() => {
+          window.open("https://wa.me/972501234567/");
+        }}
+      />
+      <img
+        className="call"
+        src={call}
+        alt="תמונה חסרה"
+        onClick={() => {
+          window.open("tel:0501234567");
+        }}
+      />
+      <img
+        className="mail"
+        src={mail}
+        alt="תמונה חסרה"
+        onClick={() => {
+          window.open("mailto:gmail@gmail.com");
+        }}
+      />
 
       <img className="imgr" src={Store} alt="תמונה חסרה" />
       <img className="imgl" src={Store} alt="תמונה חסרה" />
